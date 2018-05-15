e1355e75dfff26562260938b039381184e5fc8f8
LGY3030 <lgy3030@gmail.com>
2018-05-16 01:42:32 +0800

    Update
    practice if else

---
+++ ifelse.py
@@ -0,0 +1,14 @@
+a=input('symbol: ')
+b=int(input('number: '))
+c=int(input('number: '))
+if a=='*':
+    print(b*c)
+elif a=='/':
+    print(b/c)
+elif a=='+':
+    print(b+c)
+elif a=='-':
+    print(b-c)
+else:
+    print('error')
+
