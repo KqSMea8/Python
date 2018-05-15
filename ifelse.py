e3e7cb37fdba1db8bfca20adbbc559107fa56e5f
LGY3030 <lgy3030@gmail.com>
2018-05-16 02:09:36 +0800

    Update
    understand "and" "or" in if statement

--- ifelse.py
+++ ifelse.py
@@ -12,3 +12,14 @@ elif a=='-':
 else:
     print('error')
 
+name=input('name: ')
+yearsold=int(input('yearsold: '))
+if name=='jimmy' and (yearsold==(21 or 22)):
+    print('That is me')
+elif name=='amy' and yearsold==23:
+    print('That is the girl')
+else:
+    print('That is not us')
+
+
+
