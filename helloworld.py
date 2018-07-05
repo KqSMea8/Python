9786d2f5344e02ec658ab7eee90d8d86ed55c295
LGY3030 <lgy3030@gmail.com>
2018-07-05 15:57:53 +0800

    Update

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,15 @@
-a=5
-b=a+5
-c="Hello"
-d='world'
-e=int(input('your lucky number: '))
-print(e)
-print(c,d,str(e))
-print(a,b)
-print(a,b,c)
-print(c,d)
-print(c+d)
-print("Hello World")
+print("helloworld")
+print('helloworld')
+print(2017)
+a=2018
+print(a)
+b="hi"
+c="hello"
+print(b)
+print(c)
+temp=b
+b=c
+c=temp
+print(b)
+print(c)
+print("helloworld"+str(a))
