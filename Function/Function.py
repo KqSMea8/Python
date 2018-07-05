a9dd62f46ab3210adf18a834f8055718d5b2e857
LGY3030 <lgy3030@gmail.com>
2018-07-05 16:47:41 +0800

    Update
    practice function in python

---
+++ Function/Function.py
@@ -0,0 +1,81 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+#This is a practice
+def printfunction():
+    print("Hi!")
+    print("I'm function")
+print("Out of function")
+
+
+# In[4]:
+
+
+printfunction()
+
+
+# In[5]:
+
+
+def doublefunction(x):
+    return 2*x
+
+
+# In[7]:
+
+
+a=doublefunction(2)
+print(a)
+
+
+# In[8]:
+
+
+def plusfunction(x,y):
+    return x+y
+
+
+# In[11]:
+
+
+b=plusfunction(2,3)
+print(b)
+
+
+# In[21]:
+
+
+def function(x):
+    print(x)
+    print("I'm in the function")
+    return 3*x
+
+
+# In[14]:
+
+
+c=function(3)
+
+
+# In[15]:
+
+
+print(c)
+
+
+# In[24]:
+
+
+def function2(x):
+    print(x)
+    print("I'm in the function")
+
+
+# In[25]:
+
+
+function2(3)
+
