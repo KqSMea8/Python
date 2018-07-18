451041a6b26254ccd91a192d919d14081e1aa3cd
LGY3030 <lgy3030@gmail.com>
2018-07-18 16:02:50 +0800

    Update

---
+++ Variable/Variable.py
@@ -0,0 +1,63 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+x=y=z=10
+
+
+# In[3]:
+
+
+print(x)
+print(y)
+print(z)
+
+
+# In[4]:
+
+
+x,y,z=1,2,3
+
+
+# In[6]:
+
+
+print(x)
+print(y)
+print(z)
+
+
+# In[7]:
+
+
+x,y=1,2
+x,y=y,x
+
+
+# In[8]:
+
+
+print(x)
+print(y)
+
+
+# In[9]:
+
+
+del x
+
+
+# In[13]:
+
+
+# This is comment
+
+'''
+This is comment
+'''
+"""
+This is comment
+"""
+
