839fcacf37112395835466744cb63a1174d72dee
LGY3030 <lgy3030@gmail.com>
2018-07-18 14:40:20 +0800

    Update
    practice Series

---
+++ Series/Series.py
@@ -0,0 +1,40 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[12]:
+
+
+b=[1,"hi",3.]
+np.array(b)
+
+
+# In[13]:
+
+
+a=pd.Series([1,"hi",3.])
+
+
+# In[14]:
+
+
+a
+
+
+# In[15]:
+
+
+a=pd.Series([1,"hi",3.],index=['a','b','c'])
+
+
+# In[16]:
+
+
+a
+
