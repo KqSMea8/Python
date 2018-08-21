d54bff43078572e25289190ee2978b7f889546b2
LGY3030 <lgy3030@gmail.com>
2018-08-21 15:34:20 +0800

    Update
    practice Function

--- Function/Function.py
+++ Function/Function.py
@@ -108,3 +108,78 @@ b=int(input('b:'))
 c=sub(a,b)
 print(c)
 
+
+# In[2]:
+
+
+def interest(a,b):
+    print(a+' is my favorite sport')
+    print(b+' is my favorite fruit')
+interest(b='banana',a='basketball')
+
+
+# In[6]:
+
+
+def interest(a,b='apple'):#如有預設的,要放在最右邊
+    print(a+' is my favorite sport')
+    print(b+' is my favorite fruit')
+    print()
+interest('basketball')
+interest(a='basketball')
+interest('basketball','banana')
+interest(a='basketball',b='banana')
+interest(b='banana',a='basketball')
+
+
+# In[9]:
+
+
+def test(a):
+    print(a)
+a=test('hi')#自動return none
+print(a)
+print(type(a))
+
+
+# In[8]:
+
+
+def test(a):
+    print(a)
+    return
+a=test('hi')
+print(a)
+print(type(a))
+
+
+# In[10]:
+
+
+def subtract(a,b):
+    x=a-b
+    return x
+a=int(input('a:'))
+b=int(input('b:'))
+print('a-b=',subtract(a,b))
+
+
+# In[13]:
+
+
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+print('1:add')
+print('2:sub')
+x=int(input('1/2:'))
+a=int(input('a:'))
+b=int(input('b:'))
+if x==1:
+    print('a+b=',add(a,b))
+elif x==2:
+    print('a-b=',sub(a,b))
+else:
+    print('error')
+
