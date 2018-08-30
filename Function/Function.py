0b2c966b5b1de7d78b53ed51aaff108d5894999a
LGY3030 <lgy3030@gmail.com>
2018-08-30 20:25:35 +0800

    Update
    practice Function

--- Function/Function.py
+++ Function/Function.py
@@ -259,3 +259,49 @@ while True:
     if repeat=='N':
         break
 
+
+# In[1]:
+
+
+def abc(x):
+    str1='banana'
+    for i in x:
+        str2=i+' loves '+str1
+        print(str2)
+x=['Jimmy','Amy','Ruby']
+abc(x)
+
+
+# In[4]:
+
+
+def kitchen(unserved,served):
+    print('廚房處理顧客所點的餐點')
+    while unserved:
+        current_meal=unserved.pop()
+        print('菜單:',current_meal)
+        served.append(current_meal)
+
+def show_unserved_meal(unserved):
+    print('===下列是尚未服務的餐點===')
+    if not unserved:
+        print('***沒有餐點***','\n')
+    for unserved_meal in unserved:
+        print(unserved_meal)
+
+def show_served_meal(served):
+    print('===下列是已經服務的餐點===')
+    if not served:
+        print('***沒有餐點***','\n')
+    for served_meal in served:
+        print(served_meal)    
+
+unserved=['大麥克','勁辣雞腿堡','麥克雞塊']
+served=[]
+show_unserved_meal(unserved)
+show_served_meal(served)
+kitchen(unserved,served)
+print('\n','===廚房處理結束===','\n')
+show_unserved_meal(unserved)
+show_served_meal(served)
+
