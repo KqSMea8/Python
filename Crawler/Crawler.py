94f80d11843aede37fae51d75c22233702f227f2
GuanYu Lin <lgy3030@gmail.com>
2018-09-16 15:55:01 +0800

    Update
    practice Crawler

--- Crawler/Crawler.py
+++ Crawler/Crawler.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[4]:
+# In[40]:
 
 
 import requests
@@ -9,12 +9,12 @@ res = requests.get("https://s.taobao.com/search?q=%E4%BF%9D%E6%BA%AB%E7%93%B6&im
 print(res.text)
 
 
-# In[18]:
+# In[41]:
 
 
 from bs4 import BeautifulSoup
 html_sample=' <html>  <body>      <h1 id="title">Hello World</h1>      <a href="#" class="link">This is link1</a>      <a href="# link2" class="link">This is link2</a>  </body> </html>'
-soup=BeautifulSoup(html_sample)
+soup=BeautifulSoup(html_sample,"html.parser")
 print(soup.text)
 
 
@@ -66,4 +66,19 @@ print(soup.select('#title'))
 
 
 print(soup.select('.link'))
+
+
+# In[25]:
+
+
+#https://www.moneydj.com/KMDJ/News/NewsViewer.aspx?a=a180a15b-9e4f-4575-b28f-927fcb5c63a3
+#https://youtu.be/CDwUsqpgYpU
+#取得理財網人氣指數
+import requests
+from bs4 import BeautifulSoup
+url='https://www.moneydj.com/InfoSvc/apis/vc'
+payload='{"counts":[{"svc":"NV","guid":"a180a15b-9e4f-4575-b28f-927fcb5c63a3"}]}'
+head={'Content-Type':'application/json'}
+res = requests.post(url,data=payload,headers=head)
+print(res.text)
 
